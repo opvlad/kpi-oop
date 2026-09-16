a0af52dfffb7bb779ffe403c86c941a266d4dd6f
opvlad <opvladq@gmail.com>
2026-09-16 14:16:41 +0000

    feat: lab3

---
+++ lab3/main.py
@@ -0,0 +1,117 @@
+import sys  # noqa
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QMenuBar
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QActionGroup, QPen, QColor, QPainter
+
+from shape_tools import Tool, TOOLS, ToolBase
+
+
+class ObjectsAction(QAction):
+    action_group = None
+
+    def __init__(self, text: str, parent, tool: ToolBase):
+        super().__init__(text, parent)
+        self._create_action_group(parent)
+        self.setCheckable(True)
+        self.setActionGroup(self.action_group)
+        self.tool = tool
+        self.triggered.connect(parent.set_current_tool)
+
+    @classmethod
+    def _create_action_group(cls, parent):
+        if cls.action_group is None:
+            cls.action_group = QActionGroup(parent)
+
+
+class ObjectsMenu:
+    def __init__(self, menubar: QMenuBar):
+        self.menu = menubar.addMenu("Об’єкти")
+
+    def add_actions(self, actions: list[QAction]):
+        for action in actions:
+            self.menu.addAction(action)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Lab 2")
+        self.resize(800, 600)
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        self.strokes = []
+        self.pen = QPen(QColor("red"), 3)
+
+        self.current_tool = TOOLS[Tool.FREEHAND]
+
+        self._create_actions()
+        self._create_menubar()
+        self._create_toolbars()
+
+    def set_current_tool(self):
+        for action in self.objects_menu.menu.actions():
+            if action.isChecked() and isinstance(action, ObjectsAction):
+                self.current_tool = action.tool
+
+    def _create_actions(self):
+        self.point_action = ObjectsAction("Крапка", self, TOOLS[Tool.FREEHAND])
+        self.point_action.setChecked(True)
+        self.line_action = ObjectsAction("Лінія", self, TOOLS[Tool.LINE])
+        self.rectangle_action = ObjectsAction(
+            "Прямокутник", self, TOOLS[Tool.RECTANGLE]
+        )
+        self.ellipse_action = ObjectsAction("Еліпс", self, TOOLS[Tool.ELLIPSE])
+
+    def _create_menubar(self):
+        menubar = self.menuBar()
+
+        file_menu = menubar.addMenu("Файл")  # noqa
+        self.objects_menu = ObjectsMenu(menubar)
+        help_menu = menubar.addMenu("Довідка")  # noqa
+
+        self.objects_menu.add_actions(
+            [
+                self.point_action,
+                self.line_action,
+                self.rectangle_action,
+                self.ellipse_action,
+            ]
+        )
+
+    def _create_toolbars(self):
+        toolbar = self.addToolBar("Shapes")
+
+        toolbar.addAction(self.point_action)
+        toolbar.addAction(self.line_action)
+        toolbar.addAction(self.rectangle_action)
+        toolbar.addAction(self.ellipse_action)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.current_tool.press(self, event.position().toPoint())
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            self.current_tool.move(self, event.position().toPoint())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.current_tool.release(self, event.position().toPoint())
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        painter.setPen(self.pen)
+
+        self.current_tool.draw_preview(self, painter)
+        self.current_tool.draw_finished(self, painter)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
---
+++ lab3/shape_tools.py
@@ -0,0 +1,174 @@
+from enum import Enum, auto  # noqa
+from abc import ABC, abstractmethod
+from contextlib import contextmanager
+from typing import Generator
+
+from PySide6.QtGui import QColor
+from PySide6.QtCore import QPoint, QRect
+from PySide6.QtGui import QPainter, QPainterPath, QBrush, Qt, QPen
+
+
+class Tool(Enum):
+    FREEHAND = auto()
+    LINE = auto()
+    RECTANGLE = auto()
+    ELLIPSE = auto()
+
+
+class ToolBase(ABC):
+    @abstractmethod
+    def press(self, widget, pos: QPoint) -> None: ...
+
+    @abstractmethod
+    def move(self, widget, pos: QPoint) -> None: ...
+
+    @abstractmethod
+    def release(self, widget, pos: QPoint) -> None: ...
+
+    @abstractmethod
+    def draw_preview(self, widget, painter: QPainter) -> None: ...
+
+    @staticmethod
+    @contextmanager
+    def black_pen(painter: QPainter) -> Generator[None, None, None]:
+        last_pen = painter.pen()
+        pen = QPen(QColor("black"), 3)
+        painter.setPen(pen)
+        yield
+        painter.setPen(last_pen)
+
+    @staticmethod
+    def draw_finished(widget, painter: QPainter) -> None:
+        for tool, args in widget.strokes:
+            if tool == Tool.FREEHAND or tool == Tool.LINE:
+                with ToolBase.black_pen(painter):
+                    painter.drawPath(args)
+
+            elif tool == Tool.RECTANGLE:
+                with ToolBase.black_pen(painter):
+                    painter.drawRect(args)
+
+            elif tool == Tool.ELLIPSE:
+                with ToolBase.black_pen(painter):
+                    brush = QBrush(QColor("yellow"))
+                    painter.setBrush(brush)
+                    painter.drawEllipse(*args)
+                    painter.setBrush(Qt.BrushStyle.NoBrush)
+
+
+class FreehandTool(ToolBase):
+    def __init__(self):
+        self.path = None
+
+    def press(self, widget, pos: QPoint) -> None:
+        self.path = QPainterPath()
+        self.path.moveTo(pos)
+
+    def move(self, widget, pos: QPoint) -> None:
+        self.path.lineTo(pos)
+        widget.update()
+
+    def release(self, widget, pos: QPoint) -> None:
+        widget.strokes.append((Tool.FREEHAND, self.path))
+        self.path = None
+        widget.update()
+
+    def draw_preview(self, widget, painter: QPainter) -> None:
+        if self.path:
+            painter.drawPath(self.path)
+
+
+class LineTool(ToolBase):
+    def __init__(self):
+        self.start = None
+        self.end = None
+
+    def press(self, widget, pos: QPoint) -> None:
+        self.start = pos
+
+    def move(self, widget, pos: QPoint) -> None:
+        self.end = pos
+        widget.update()
+
+    def release(self, widget, pos: QPoint) -> None:
+        path = QPainterPath()
+        path.moveTo(self.start)
+        path.lineTo(pos)
+        widget.strokes.append((Tool.LINE, path))
+        self.start, self.end = None, None
+        widget.update()
+
+    def draw_preview(self, widget, painter: QPainter) -> None:
+        if self.start and self.end:
+            painter.drawLine(self.start, self.end)
+
+
+class RectTool(ToolBase):
+    def __init__(self):
+        self.rect = None
+        self.center = None
+        self.start = None
+        self.end = None
+
+    def press(self, widget, pos: QPoint) -> None:
+        self.center = pos
+
+    def move(self, widget, pos: QPoint) -> None:
+        half_width = abs(self.center.x() - pos.x())
+        half_height = abs(self.center.y() - pos.y())
+        self.start = QPoint(self.center.x() - half_width, self.center.y() - half_height)
+        self.end = QPoint(self.center.x() + half_width, self.center.y() + half_height)
+        widget.update()
+
+    def release(self, widget, pos: QPoint) -> None:
+        self.rect = QRect(self.start, self.end)
+        widget.strokes.append((Tool.RECTANGLE, self.rect))
+        self.rect, self.center, self.start, self.end = None, None, None, None
+        widget.update()
+
+    def draw_preview(self, widget, painter: QPainter) -> None:
+        if self.start and self.end:
+            rect = QRect(self.start, self.end)
+            painter.drawRect(rect)
+
+
+class EllipseTool(ToolBase):
+    def __init__(self):
+        self.start = None
+        self.end = None
+        self.center = None
+        self.rx = None
+        self.ry = None
+
+    def press(self, widget, pos: QPoint) -> None:
+        self.start = pos
+
+    def move(self, widget, pos: QPoint) -> None:
+        self.end = pos
+        self.rx = (self.end.x() - self.start.x()) / 2
+        self.ry = (self.end.y() - self.start.y()) / 2
+        self.center = QPoint(self.start.x() + self.rx, self.start.y() + self.ry)
+        widget.update()
+
+    def release(self, widget, pos: QPoint) -> None:
+        widget.strokes.append((Tool.ELLIPSE, (self.center, self.rx, self.ry)))
+        self.start, self.end, self.center, self.rx, self.ry = (
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        widget.update()
+
+    def draw_preview(self, widget, painter: QPainter) -> None:
+        if self.center and self.rx and self.ry:
+            painter.drawEllipse(self.center, self.rx, self.ry)
+
+
+TOOLS = {
+    Tool.FREEHAND: FreehandTool(),
+    Tool.LINE: LineTool(),
+    Tool.RECTANGLE: RectTool(),
+    Tool.ELLIPSE: EllipseTool(),
+}
